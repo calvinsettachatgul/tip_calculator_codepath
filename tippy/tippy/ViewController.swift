412fb6002b2dc11dd5a0491b83b573a70261f842
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2017-03-01 21:46:34 -0800

    add tap gesture recognizer

---
+++ tippy/tippy/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Calvin Settachatgul on 2/28/17.
+//  Copyright © 2017 Calvin Settachatgul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onTap(_ sender: Any) {
+        print("hello")
+    }
+
+}
+

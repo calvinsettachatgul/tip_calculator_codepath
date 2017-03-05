b8692b1acffad6b46985a404a53daa1bc7788135
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2017-03-04 20:36:23 -0800

    complete

--- tippy/tippy/ViewController.swift
+++ tippy/tippy/ViewController.swift
@@ -42,5 +42,22 @@ class ViewController: UIViewController {
         tipLabel.text = String( format: "$%.2f", tip)
         totalLabel.text = String( format: "$%.2f", total)
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("view did disappear")
+    }
 }
 
